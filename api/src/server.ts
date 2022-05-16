bab0520f6b2b6b893e1960328b59a2c93b160621
bilal musani <bilal.musani@tum.de>
2022-05-16 21:41:54 +0200

    Integrate error handling middleware

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import jobsRouter from "./routes/job.routes";
 import dotenv from 'dotenv';
+import { isOperationalError, logError, logErrorMiddleware, returnError } from './utils/handlers/error.handler';
+
 dotenv.config();
 
 const app = express();
@@ -14,3 +16,21 @@ app.use(bodyParser.json());
 app.use('/jobs', jobsRouter);
 
 app.listen(PORT);
+
+
+// Add middleware to catch exceptions thrown in the app
+app.use(logErrorMiddleware)
+app.use(returnError)
+
+// Handle all unmanage promise exceptions by throwing errors to be caught in the subsequent block
+process.on('unhandledRejection', error => {
+    throw error
+})
+
+// Manage all uncaught exceptions
+process.on('uncaughtException', error => {
+    logError(error)
+    if (!isOperationalError(error)) {
+        process.exit(1)
+    }
+})
