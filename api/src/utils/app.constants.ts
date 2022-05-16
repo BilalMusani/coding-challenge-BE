318ffb2eaf0054095e067a56767eb3e2cd00c177
bilal musani <bilal.musani@tum.de>
2022-05-16 20:43:58 +0200

    Add mailing constants

---
+++ api/src/utils/app.constants.ts
@@ -0,0 +1,5 @@
+export class MailingConstants {
+    public static DEFAULT_SALUTATION: string = 'Hi';
+    public static NEW_JOB_POSTED_SUBJECT: string = 'A new job has been posted';
+    public static NEW_JOB_POSTED_TOPIC: string = 'This is to inform you that a new job has been posted that fits your requirements.';
+}
