4bc7a489a0219bc0df664cda65dc9dc7ad66119e
bilal musani <bilal.musani@tum.de>
2022-05-16 20:44:36 +0200

    Add error messages

---
+++ api/src/utils/errors/error.messages.ts
@@ -0,0 +1,5 @@
+export class ErrorMessages {
+    public static DEFAULT: string = 'The app has encountered an error.';
+    public static NETWORK_ERROR: string = 'Application encountered a network error performing the query';
+    public static UNABLE_TO_RETRIEVE_USERS: string = 'Could not retrieve users!';
+}
