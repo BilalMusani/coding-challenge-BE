c6d8b74ebfd1db0a52be2e162ae9d3d245174942
bilal musani <bilal.musani@tum.de>
2022-05-16 21:50:42 +0200

    Add user queries

--- api/src/queries/users.ts
+++ api/src/queries/users.ts
@@ -1,15 +1,14 @@
 import gql from "graphql-tag";
 
-export let QUERY_USERS = (condition: string) => gql`
-    query fetchUsers {
-        user (where: ({${condition}}) {
-            email
-            id
-            keywords
-            send_digest
-            cities
-        }
-    }
-`;
-
-export let WHERE_CLAUSE = (col: string, condition: any) => `{${col}: {${condition}}}`
+export let GET_USERS_ADVANCED = gql`
+query getUsersAdvanced {
+  get_users_advanced {
+    city
+    company_name
+    email
+    id
+    job_id
+    investor_name
+    job_title
+  }
+}`;
