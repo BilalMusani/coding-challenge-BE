8c8eeed899863329fb8bf3a99714660d87f26ff7
bilal musani <bilal.musani@tum.de>
2022-05-16 20:44:27 +0200

    Add baseerror class

---
+++ api/src/utils/errors/base.error.ts
@@ -0,0 +1,14 @@
+class BaseError extends Error {
+    constructor(
+        public name: string,
+        public statusCode: any,
+        public isOperational: any,
+        public description: string)
+    {
+        super(description)
+        Object.setPrototypeOf(this, new.target.prototype)
+        Error.captureStackTrace(this)
+    }
+}
+
+module.exports = BaseError
