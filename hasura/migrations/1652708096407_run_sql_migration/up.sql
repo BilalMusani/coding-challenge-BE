45bed7328c209e0f8f1c3ee49ff0cb192ae5a1a5
bilal musani <bilal.musani@tum.de>
2022-05-16 15:54:20 +0200

    Add function to get users matching a job

---
+++ hasura/migrations/1652708096407_run_sql_migration/up.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION get_users_matching_job(title text, city text) 
+returns setof public.user AS
+$$
+    SELECT   distinct u.id, u.email, u.cities, u.send_digest, u.keyword_id, u.email_sent_date
+    FROM     public.user u
+    JOIN user_to_keywords uk
+    ON uk.user_id = u.id
+    JOIN keywords k
+    ON k.id = uk.keyword_id
+    WHERE to_tsvector(title) @@ to_tsquery('english', REPLACE(k.keyword, ' ','<->'))
+    AND u.cities ILIKE '%' || city || '%'
+    AND u.send_digest = False;
+$$ language sql stable;
+;
