188554ffdb94aee269c0e448b21345a09b520203
bilal musani <bilal.musani@tum.de>
2022-05-16 21:50:32 +0200

    Add job relevant queries

--- api/src/queries/jobs.ts
+++ api/src/queries/jobs.ts
@@ -1,12 +1,33 @@
 import gql from "graphql-tag";
 
 
-export const QUERY_JOBS = gql`
-    query fetchJobs {
-        jobs {
-            id
-            title
-            city
-        }
+export const GET_USERS_MATCHING_JOB = gql`
+query getUsersMatchingJob($title: String!, $city: String!) {
+    get_users_matching_job(args: {title: $title, city: $city}) {
+      cities
+      email
+      email_sent_date
+      id
+      keyword_id
+      send_digest
     }
-`;
+}`;
+
+export const GET_JOBS_COMPANY_INVESTORS = gql`
+query getJobCompanyAndInvestor($id: Int!) {
+    jobs(where: {id: {_eq: $id}}) {
+      company {
+        id
+        name
+        company_investors {
+          investor {
+            id
+            name
+          }
+        }
+      }
+      id
+      title
+      city
+    }
+}`;
