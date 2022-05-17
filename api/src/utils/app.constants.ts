ca22a268f7c383950b7db0cc7e2cdca9dc9b5608
bilal musani <bilal.musani@tum.de>
2022-05-17 22:35:37 +0200

    Update mail constants

--- api/src/utils/app.constants.ts
+++ api/src/utils/app.constants.ts
@@ -2,4 +2,6 @@ export class MailingConstants {
     public static DEFAULT_SALUTATION: string = 'Hi';
     public static NEW_JOB_POSTED_SUBJECT: string = 'A new job has been posted';
     public static NEW_JOB_POSTED_TOPIC: string = 'This is to inform you that a new job has been posted that fits your requirements.';
+    public static MESSAGE_DIGEST_SUBJECT: string = 'Your daily digest';
+    public static MESSAGE_DIGEST_TOPIC: string = 'Here are your job recommendations for today.';
 }
