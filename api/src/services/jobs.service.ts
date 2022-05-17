952533cb0bf9505ae0e5490fe988a97da3f9fbfc
bilal musani <bilal.musani@tum.de>
2022-05-17 22:34:39 +0200

    Add send digest method in mailing service

--- api/src/services/jobs.service.ts
+++ api/src/services/jobs.service.ts
@@ -1,12 +1,25 @@
-import { GetJobCompanyAndInvestorQuery, Jobs, User } from '../generated/graphql';
+import { GET_USERS_ADVANCED_QUERY } from '../queries/users';
+import { apolloClient } from '../apollo';
+import {
+    GetJobCompanyAndInvestorQuery,
+    GetUsersAdvancedQuery,
+    GetUsersAdvancedQueryVariables,
+    Jobs,
+    User,
+    Get_Users_Advanced,
+    Company_Investors
+} from '../generated/graphql';
 import { MailingConstants } from '../utils/app.constants';
 import { MailingService } from './mailing.service';
-
+import { logError } from '../utils/handlers/error.handler';
+import { getMessageFromApolloResult } from '../utils/errors/error.utils';
+import * as _ from 'lodash';
+import Mail from 'nodemailer/lib/mailer';
 
 export class JobsService {
     constructor(
         private mailingService: MailingService = new MailingService(),
-    ) {}
+    ) { }
 
     private combineInvestors(job: Partial<Jobs>): string {
         return job.company.company_investors.map(x => x.investor.name)
@@ -17,13 +30,13 @@ export class JobsService {
         return '<li> ' + job.title + ' - ' + job.company.name + '(' + this.combineInvestors(job) + ')' + ' located in ' + '<b>' + job.city + '</b>.' + '</li>';
     }
 
-    private createHtmlList(job: any): string {
-        return  '<h1>' + MailingConstants.DEFAULT_SALUTATION + ',</h1>' +
+    private createHtmlList(salutation: string, topic: string, jobs: any[]): string {
+        return  '<h1>' + salutation + ',</h1>' +
                 '<br><br>' +
                 '<div>' +
-                    '<p>' + MailingConstants.NEW_JOB_POSTED_TOPIC + '</p>' +
+                    '<p>' + topic + '</p>' +
                     '<ul>' +
-                        this.createHtmlListItem(job) +
+                        jobs.map(job => this.createHtmlListItem(job)).reduce((acc, jobListItem) => acc + jobListItem, '') +
                     '</ul>' +
                 '</div>';
     }
@@ -34,15 +47,104 @@ export class JobsService {
 
     singleJobPosting(user: Partial<User>[], jobResponse: GetJobCompanyAndInvestorQuery["jobs"]): Promise<any> {
         const job: any = jobResponse[0];
-        const html: string = this.createHtmlList(job);
-        const plaintext =  MailingConstants.DEFAULT_SALUTATION + ", " + MailingConstants.NEW_JOB_POSTED_TOPIC + "; " + this.createPlainText(job);
+        const html: string = this.createHtmlList(MailingConstants.DEFAULT_SALUTATION, MailingConstants.NEW_JOB_POSTED_TOPIC, [job]);
+        const plaintext = MailingConstants.DEFAULT_SALUTATION + ", " + MailingConstants.NEW_JOB_POSTED_TOPIC + "; " + this.createPlainText(job);
         const mailOptions = {
             to: user.map(x => x.email),
             subject: MailingConstants.NEW_JOB_POSTED_SUBJECT, // Subject line
             text: plaintext, // plaintext body
             html,
         };
-        return this.mailingService.sendEmail(mailOptions);
+        return this.mailingService.sendEmail([mailOptions]);
+    }
+
+    async sendDigest() {
+        const data = await apolloClient.query<GetUsersAdvancedQuery, GetUsersAdvancedQueryVariables>({
+            query: GET_USERS_ADVANCED_QUERY,
+            // This query is run via cron job so no need to cache
+            fetchPolicy: 'network-only'
+        });
+
+        if (data.error || data.errors || data.networkStatus === 8) {
+            return logError(getMessageFromApolloResult(data));
+        }
+
+        if (data.data && data.data.get_users_advanced.length > 0) {
+            const users: { [key: string]: Jobs[] } = {}
+            const jobs: { [key: string]: Jobs } = {}
+            const companyToInvestorDict: { [key: string]: Set<string> } = {}
+
+            data.data.get_users_advanced.forEach(user => {
+                if(!companyToInvestorDict.hasOwnProperty(user.company_name)) {
+                    companyToInvestorDict[user.company_name] = new Set<string>();
+                }
+                companyToInvestorDict[user.company_name].add(user.investor_name);
+            });
+
+            data.data.get_users_advanced.forEach(user => {
+                if(!users.hasOwnProperty(user.email)) {
+                    users[user.email] = [];
+                }
+                if(!jobs.hasOwnProperty(user.job_id)) {
+                    jobs[user.job_id] = this.createJob(user, [...companyToInvestorDict[user.company_name]]);
+                }
+                users[user.email].push(jobs[user.job_id]);
+            });
+
+            const mailOptions: Mail.Options[] = [];
+
+            Object.keys(users).forEach(email => {
+                const distinctJobs = _.uniqBy(users[email], (job) => job.id);
+                let plaintext = '';
+                distinctJobs.forEach(job => plaintext += this.createPlainText(job) + ' ');
+                mailOptions.push({
+                    to: email,
+                    subject: MailingConstants.MESSAGE_DIGEST_SUBJECT, // Subject line
+                    text: plaintext.trimEnd(), // plaintext body
+                    html: this.createHtmlList(MailingConstants.DEFAULT_SALUTATION, MailingConstants.MESSAGE_DIGEST_TOPIC, distinctJobs),
+                });
+            });
+            return this.mailingService.sendEmail(mailOptions);
+        }
+    }
+
+    /**
+     *
+     * @param jobsGrouping: Dictionary Mapping of id to Get_User_Advanced query return object grouped by job id
+     * @param jobKey: Job Id
+     * @param job: Individual Job Instance
+     * @returns Job object with the relevant fields populated to construct an email
+     */
+    private createJob(
+        job: Get_Users_Advanced,
+        companyInvestors: string[]): Jobs {
+        return {
+            company: {
+                __typename: 'companies',
+                company_investors: [...companyInvestors.map(investorName => (
+                    {
+                        company: null,
+                        company_id: null,
+                        investor: {
+                            __typename: 'investors',
+                            company_investors: [],
+                            company_investors_aggregate: null,
+                            id: 0,
+                            name: investorName,
+                        },
+                        investor_id: 0
+                    }) as Company_Investors)],
+                company_investors_aggregate: null,
+                id: job.job_id,
+                jobs: [],
+                jobs_aggregate: null,
+                name: job.company_name,
+            },
+            company_id: 0,
+            id: job.id,
+            title: job.job_title,
+            city: job.city
+        };
     }
 }
 
